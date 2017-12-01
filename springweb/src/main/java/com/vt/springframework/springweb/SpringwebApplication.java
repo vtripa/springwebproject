f539e9ad082f61ef6f94d0f522ff0c9f0db39821
129472 <129472@INDSNOIDV3K622.AMERICAS.GLOBAL.NTTDATA.COM>
2017-12-01 13:16:58 +0530

    Initial Spring Boot project setup

---
+++ springweb/src/main/java/com/vt/springframework/springweb/SpringwebApplication.java
@@ -0,0 +1,15 @@
+package com.vt.springframework.springweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by vt on 11/30/2017.
+ */
+@SpringBootApplication
+public class SpringwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebApplication.class, args);
+	}
+}
